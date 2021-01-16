8dbbc404144ce05291b15ea875b82f96d9ac03af
Matthew Eckes <eckesm@gmail.com>
2021-01-16 18:17:23 -0500

    incorporating suggested solutions

--- app.js
+++ app.js
@@ -10,14 +10,18 @@ new Set([1,1,2,2,3,4])     */
 // ----- Quick Question #2 -----
 /* What does the following code return?
 [...new Set("referee")].join("")      */
-// ANSWER ---> 'referee'
+// ANSWER ---> 'referee'   WRONG
+// SOLUTION ---> 'ref'   I forgot to remove the duplicate letters
 
 // ----- Quick Questions #3
 /* What does the Map m look like after running the following code?
 let m = new Map();
 m.set([1,2,3], true);
 m.set([1,2,3], false);     */
-// ANSWER ---> [[1,2,3],false]
+// ANSWER ---> [[1,2,3],false]   WRONG
+/*  SOLUTION --->
+0: {Array(3) => true}
+1: {Array(3) => false}   BECAUSE the arrays are not the same */
 
 // ______________________________________________
 
@@ -35,10 +39,9 @@ hasDuplicate([1,5,-1,4]) // false     */
 //   }
 // }
 
-const hasDuplicate = arr =>
+// const hasDuplicate = arr => arr.length !== new Set([ ...arr ]).size ? true : false;     <--- my original method
 
-		arr.length !== new Set([ ...arr ]).size ? true :
-		false;
+const hasDuplicate = arr => arr.length !== new Set([ ...arr ]).size; // <--- incorporating suggested soltion (don't need the true/false)
 
 // ----- vowelCount -----
 /* Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values are the count of the vowels in the string.
@@ -46,18 +49,37 @@ const hasDuplicate = arr =>
 vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
 vowelCount('Colt') // Map { 'o' => 1 }     */
 
+// my original method
+// const vowelCount = str => {
+// 	const vowelArr = [ ...str ];
+//   const vowelMap = new Map();
+//   const vowels ="aeiou"
+// 	vowelArr.forEach(char => {
+//     if (vowels.includes(char)) {
+//       if (vowelMap.has(char)) {
+//         vowelMap.set(char, vowelMap.get(char) + 1);
+//       } else {
+//         vowelMap.set(char, 1);
+//       }
+//     }
+// 	});
+// 	return vowelMap;
+// };
+
+// incorporating suggested solutions
 const vowelCount = str => {
-	const vowelArr = [ ...str ];
-  const vowelMap = new Map();
-  const vowels ="aeiou"
-	vowelArr.forEach(char => {
-    if (vowels.includes(char)) {
-      if (vowelMap.has(char)) {
-        vowelMap.set(char, vowelMap.get(char) + 1);
-      } else {
-        vowelMap.set(char, 1);
-      }
-    }
-	});
+	const vowelMap = new Map();
+	const vowels = 'aeiou';
+
+	for (let char of str) {
+		let lowerCaseChar = char.toLowerCase();
+		if (vowels.includes(lowerCaseChar)) {
+			if (vowelMap.has(lowerCaseChar)) {
+				vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+			} else {
+				vowelMap.set(lowerCaseChar, 1);
+			}
+		}
+	}
 	return vowelMap;
 };
